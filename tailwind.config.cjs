fcf362fa9945805509a01b5a599611245d0698f2
Vyast <noahbaca2002@icloud.com>
2022-09-21 19:53:33 -0600

    add custom fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "n4-aqua": "#00feff",
+        "n4-darkblue": "#014188",
+        "n4-gray": "#818181",
+      },
+      animation: {
+        "gradient-text": "gradient-text 5s ease infinite",
+      },
+      keyframes: {
+        "gradient-text": {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+      },
+    },
   },
-  plugins: [require("prettier-plugin-tailwindcss")],
+  plugins: [],
 };
