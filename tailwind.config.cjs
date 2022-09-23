4c3acfaf75f6ac7bbad46a41e2b05570d864f6bf
Vyast <noahbaca2002@icloud.com>
2022-09-22 18:27:49 -0600

    trying a different method of using custom font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      fontFamily: {
+        blenderPro: ["BlenderPro", "sans-serif"],
+      },
       colors: {
         "n4-aqua": "#00feff",
         "n4-darkblue": "#014188",
