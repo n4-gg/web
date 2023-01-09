aa44c78823f5c59be1f91b58da369b321e072d15
Noah <noah@n4.gg>
2023-01-08 17:18:44 -0700

    create next 13 app

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,37 +1,8 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        blenderPro: ["BlenderPro", "sans-serif"],
-      },
-      colors: {
-        "n4-aqua": "#00feff",
-        "n4-darkblue": "#014188",
-        "n4-gray": "#818181",
-      },
-      backgroundImage: {
-        "hero-pattern": "url('/circuit-board.svg')",
-      },
-      animation: {
-        text: "text 5s ease infinite",
-      },
-      keyframes: {
-        text: {
-          "0%, 100%": {
-            "background-size": "200% 200%",
-            "background-position": "left center",
-          },
-          "50%": {
-            "background-size": "200% 200%",
-            "background-position": "right center",
-          },
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
