1ce588582198a392811b4fa28ee49df92fdb8c00
Noah <noah@n4.gg>
2023-01-09 16:35:18 -0700

    show vercel env in ping route to test

--- src/pages/api/ping.ts
+++ src/pages/api/ping.ts
@@ -1,7 +1,18 @@
+import { NextResponse } from "next/server";
+
 export const config = {
   runtime: "edge",
 };
 
 export default function handler() {
-  return new Response("Pong!", { status: 200 });
+  return NextResponse.json(
+    {
+      VERCEL: process.env.VERCEL,
+      CI: process.env.CI,
+      VERCEL_ENV: process.env.VERCEL_ENV,
+      VERCEL_URL: process.env.VERCEL_URL,
+      VERCEL_REGION: process.env.VERCEL_REGION,
+    },
+    { status: 200 }
+  );
 }
